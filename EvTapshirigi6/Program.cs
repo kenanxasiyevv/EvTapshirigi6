6c61f32d8041c93eac4f4d753dd8b3692650da85
Kanan Xasiyev <121182410+kenanxasiyevv@users.noreply.github.com>
2023-01-06 20:03:41 +0400

    Update Program.cs

--- EvTapshirigi6/Program.cs
+++ EvTapshirigi6/Program.cs
@@ -3,8 +3,28 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   //Cut yerde olan tek elementlerin sayi ve cemi
+            
+            Console.Write("Massivin uzunlugunu daxil edin: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"arr[{i}]= ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 != 0 && arr[i] % 2 != 0)
+                {
+                    count++;
+                    sum += arr[i];
+                }
+            }
+            Console.WriteLine($"cem: {sum} say: {count}");
         }
     }
 }
